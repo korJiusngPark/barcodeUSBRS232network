9e9c3bc218fb13490bc918c9a2f7ea4860bedfe3
jisung Park <0831park@gmail.com>
2022-03-02 16:34:25 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace barcodeUSBRS232network
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        public static string folderPath = "C:/airLeak/Result/";
+        public static string barcode;
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxSerial = new System.Windows.Forms.TextBox();
+            this.textBoxBarcode = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // textBoxSerial
+            // 
+            this.textBoxSerial.Location = new System.Drawing.Point(38, 21);
+            this.textBoxSerial.Name = "textBoxSerial";
+            this.textBoxSerial.Size = new System.Drawing.Size(225, 21);
+            this.textBoxSerial.TabIndex = 0;
+            // 
+            // textBoxBarcode
+            // 
+            this.textBoxBarcode.Location = new System.Drawing.Point(107, 124);
+            this.textBoxBarcode.Name = "textBoxBarcode";
+            this.textBoxBarcode.Size = new System.Drawing.Size(100, 21);
+            this.textBoxBarcode.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 248);
+            this.Controls.Add(this.textBoxBarcode);
+            this.Controls.Add(this.textBoxSerial);
+            this.Name = "Form1";
+            this.Text = "AirLeak";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSerial;
+        private System.Windows.Forms.TextBox textBoxBarcode;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox_received;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;  //시리얼통신을 위해 추가해줘야 함
+using System.IO;
+
+namespace barcodeUSBRS232network
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Serial_connect()
+        {
+                string port = "COM6";
+                serialPort1.PortName = port;  //콤보박스의 선택된 COM포트명을 시리얼포트명으로 지정
+                serialPort1.BaudRate = 9600;  //보레이트 변경이 필요하면 숫자 변경하기
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Parity = Parity.None;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived); //이것이 꼭 필요하다
+
+                serialPort1.Open();  //시리얼포트 열기
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // string folderPath = "C:/airLeak/Result";
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+
+            if(di.Exists == false)
+            {
+                di.Create();
+            }
+            // Serial_connect();
+        }
+
+        private void save_csv(string s)
+        {
+            string resultFolderPath = folderPath + DateTime.Now.ToString("yyMMdd");
+            DirectoryInfo di = new DirectoryInfo(resultFolderPath);
+
+            if (di.Exists == false)
+            {
+                di.Create();
+            }
+
+            string fileNmae = resultFolderPath+"/"+ DateTime.Now.ToString("yyMMdd") + ".csv";
+
+            string[] temps = s.Split('\t');
+            List<string> word = new List<string>();
+            word.Add(barcode);
+
+            foreach (string temp in temps)
+            {
+                if (temp != "")
+                    word.Add(temp); //6개 바코드 추가하면 7개
+            }
+
+            FileStream file = new FileStream(fileNmae, FileMode.Append, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(file, Encoding.UTF8);
+
+            //if (file.Length == 0)
+
+            sw.WriteLine("{0},{1},{2},{3},{4},{5},{6]", word[0], word[1], word[2], word[3], word[4], word[5],word[6]);
+            // using (System.IO.StreamReader file = new System.IO.StreamReader())
+
+
+            sw.Flush();
+            sw.Close();
+            file.Close();
+        }
+
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)  //수신 이벤트가 발생하면 이 부분이 실행된다.
+        {
+            this.Invoke(new EventHandler(MySerialReceived));  //메인 쓰레드와 수신 쓰레드의 충돌 방지를 위해 Invoke 사용. MySerialReceived로 이동하여 추가 작업 실행.
+        }
+
+        private void MySerialReceived(object s, EventArgs e)  //여기에서 수신 데이타를 사용자의 용도에 따라 처리한다.
+        {
+            string rcvData_byte_format = string.Empty;
+            string rcvData_ascii_format = string.Empty;
+
+            while (serialPort1.BytesToRead != 0)
+            {
+                var num = 0;
+                int[] arr_rcvData_bytes = new int[7];
+                char[] arr_rcvData_char = new char[7];
+                while (num < 7)
+                {
+                    arr_rcvData_bytes[num] = serialPort1.ReadByte();
+                    arr_rcvData_char[num] = Convert.ToChar(arr_rcvData_bytes[num]);
+
+                    rcvData_byte_format += arr_rcvData_bytes[num].ToString("X2");
+                    rcvData_ascii_format += arr_rcvData_char[num];
+
+                    num++;
+                }
+            }
+            save_csv(rcvData_ascii_format);
+            textBoxBarcode.Clear();
+            textBoxBarcode.Focus();
+            //richTextBox_received.AppendText("Ascii Format : " + rcvData_ascii_format + "\r\n");
+        }
+
+        
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            barcode = textBoxSerial.Text.Trim().ToString();
+            save_csv("11");
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            barcode = textBoxSerial.Text.Trim().ToString();
+            save_csv("11");
+        }
+    }
+}
